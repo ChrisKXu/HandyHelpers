9bb82399e4ce55a529a6a602f5533d06e4ff17d5
Christopher Xu <chriskxu.tictac@gmail.com>
2016-03-07 14:00:40 +0800

    using dnx-mono in travis

--- scripts/envsetup.sh
+++ scripts/envsetup.sh
@@ -2,7 +2,7 @@
 
 # Setup dnx
 curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh && source ~/.dnx/dnvm/dnvm.sh
-dnvm install latest -r clr -arch x86 -alias default
+dnvm install latest
 
 # Restore dependencies
 dnu restore
