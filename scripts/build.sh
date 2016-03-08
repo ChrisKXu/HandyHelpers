bb4a2772570f60250ad29f2bd94b3afe10b0018c
Christopher Xu <chriskxu.tictac@gmail.com>
2016-03-08 14:17:53 +0800

    fix build hang in travis

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,7 +11,9 @@ dnu restore --quiet
 dnu build ./HandyHelpers/project.json ./HandyHelpers.Tests/project.json --quiet
 
 # Test
-dnx -p HandyHelpers.Tests test
+# Appending "-parallel none" to workaround a known bug that causes dnx to hang when using mono
+# See http://stackoverflow.com/questions/33581380/dnx-test-on-travis-with-mono-hangs
+dnx -p HandyHelpers.Tests test -parallel none
 
 # All clear
 echo "All clear!"
