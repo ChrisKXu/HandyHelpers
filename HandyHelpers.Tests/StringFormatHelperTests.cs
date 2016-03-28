eff6eefe1c6b5fbee133afcbbe69a2eab779aef8
Christopher Xu <chriskxu.tictac@gmail.com>
2016-03-28 15:20:05 +0800

    Adding StringFormatHelpers, basic support

---
+++ HandyHelpers.Tests/StringFormatHelperTests.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+
+namespace HandyHelpers.Tests
+{
+    using HandyHelpers;
+
+    using ArgsDictionary = Dictionary<string, object>;
+    using TupleWithDictionary = Tuple<string, Dictionary<string, object>, string>;
+    using TupleWithObject = Tuple<string, object, string>;
+
+    public class StringFormatHelperTests
+    {
+        [Fact]
+        public void TestStringFormatWithDictionarySimple()
+        {
+            var testData = new List<TupleWithDictionary>()
+            {
+                new TupleWithDictionary("{foo}", new ArgsDictionary() { { "foo", "bar" } }, "bar")
+            };
+
+            testData.Should().NotBeEmpty();
+
+            foreach (var testCase in testData)
+            {
+                // run the test case
+                testCase.Item1.Format(testCase.Item2).Should().Be(testCase.Item3);
+            }
+        }
+
+        [Fact]
+        public void TestStringFormatWithObjectSimple()
+        {
+            var testData = new List<TupleWithObject>()
+            {
+                new TupleWithObject("{foo}", new { foo = "bar" }, "bar")
+            };
+
+            testData.Should().NotBeEmpty();
+
+            foreach (var testCase in testData)
+            {
+                // run the test case
+                testCase.Item1.Format(testCase.Item2).Should().Be(testCase.Item3);
+            }
+        }
+
+        [Theory]
+        [InlineData("", "", new string[] { })]
+        [InlineData("{foo}", "{0}", new[] { "foo" })]
+        [InlineData("a{foo}b", "a{0}b", new[] { "foo" })]
+        [InlineData("a{foo}b{bar}c", "a{0}b{1}c", new[] { "foo", "bar" })]
+        public void TestTryParseAndConvertTemplate(string template, string expectedConvertedTemplate, ICollection<string> expectedKeysArray)
+        {
+            var actualTemplate = string.Empty;
+            ICollection<string> actualKeysArray = null;
+
+            StringFormatHelpers.TryParseAndConvertTemplate(template, out actualTemplate, out actualKeysArray);
+
+            actualTemplate.Should().Be(expectedConvertedTemplate);
+            actualKeysArray.Should().Equal(expectedKeysArray);
+        }
+
+        [Fact]
+        public void TestExtractObjectFromPropertyPathSimple()
+        {
+            var testObj = new
+            {
+                foo = "bar",
+                nested = new
+                {
+                    foo = "bar"
+                }
+            };
+
+            StringFormatHelpers.ExtractObjectFromPropertyPath("foo", testObj).Should().Be(testObj.foo);
+            StringFormatHelpers.ExtractObjectFromPropertyPath("nested.foo", testObj).Should().Be(testObj.nested.foo);
+        }
+
+        private class A
+        {
+            public B B { get; set; } 
+
+            public int Value { get; set; }
+        }
+
+        private class B
+        {
+            public A A { get; set; }
+
+            public int Value { get; set; }
+        }
+
+        [Fact]
+        public void TestExtractObjectFromPropertyPathCircular()
+        {
+            var a = new A() { Value = 1 };
+            var b = new B() { Value = 2, A = a };
+            a.B = b;
+
+            StringFormatHelpers.ExtractObjectFromPropertyPath("Value", a).Should().Be(a.Value);
+            StringFormatHelpers.ExtractObjectFromPropertyPath("Value", b).Should().Be(b.Value);
+
+            StringFormatHelpers.ExtractObjectFromPropertyPath("B.Value", a).Should().Be(a.B.Value);
+            StringFormatHelpers.ExtractObjectFromPropertyPath("A.Value", b).Should().Be(b.A.Value);
+
+            StringFormatHelpers.ExtractObjectFromPropertyPath("A.B.A.B.A.Value", b).Should().Be(a.Value);
+        }
+    }
+}
---
+++ HandyHelpers/StringFormatHelpers.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Reflection;
+
+namespace HandyHelpers
+{
+    /// <summary>
+    /// Helper methods for formating strings in different ways
+    /// </summary>
+    public static class StringFormatHelpers
+    {
+        public static string Format(this string template, IDictionary<string, object> properties)
+        {
+            return Format(template, properties, null);
+        }
+
+        /// <summary>
+        /// Replaces the format item or items in a specified string with the string representation of the corresponding object. 
+        /// </summary>
+        /// <param name="template">
+        ///   A composite format string, in which the placeholders should be in the form of {index,align:format},
+        ///   where index is the key in the properties dictionary, and align and format in the same form as in string.Format
+        /// </param>
+        /// <param name="properties">A property bag containing zero or more objects to format</param>
+        /// <param name="formatProvider">An object that supplies culture-specific formatting information</param>
+        /// <returns>A copy of format in which the format item or items have been replaced by the string representation of the values in the property bag. </returns>
+        public static string Format(this string template, IDictionary<string, object> properties, IFormatProvider formatProvider)
+        {
+            if (string.IsNullOrEmpty(template))
+            {
+                // throw NullReferenceException instead of ArgumentNullException to mimic the bahavior of an instance method
+                throw new NullReferenceException("string template cannot be null");
+            }
+
+            if (properties == null)
+            {
+                throw new ArgumentNullException(nameof(properties));
+            }
+
+            var convertedTemplate = string.Empty;
+            ICollection<string> propertyIndexes = null;
+
+            TryParseAndConvertTemplate(template, out convertedTemplate, out propertyIndexes);
+
+            var args = propertyIndexes.Select(key => properties[key]).ToArray();
+
+            return string.Format(formatProvider, convertedTemplate, args);
+        }
+
+        public static string Format(this string template, object objectToFormat)
+        {
+            return Format(template, objectToFormat, null);
+        }
+
+        public static string Format(this string template, object objectToFormat, IFormatProvider formatProvider)
+        {
+            if (string.IsNullOrEmpty(template))
+            {
+                // throw NullReferenceException instead of ArgumentNullException to mimic the bahavior of an instance method
+                throw new NullReferenceException("string template cannot be null");
+            }
+
+            if (objectToFormat == null)
+            {
+                throw new ArgumentNullException(nameof(objectToFormat));
+            }
+
+            var convertedTemplate = string.Empty;
+            ICollection<string> propertyIndexes = null;
+
+            TryParseAndConvertTemplate(template, out convertedTemplate, out propertyIndexes);
+
+            var args = propertyIndexes.Select(key => ExtractObjectFromPropertyPath(key, objectToFormat)).ToArray();
+
+            return string.Format(formatProvider, convertedTemplate, args);
+        }
+
+        /// <summary>
+        /// Convert the template into string.Format template and return the referenced property indexes in order
+        /// </summary>
+        /// <param name="template"></param>
+        /// <param name="convertedTemplate"></param>
+        /// <param name="propertyKeysInOrder"></param>
+        internal static void TryParseAndConvertTemplate(string template, out string convertedTemplate, out ICollection<string> propertyKeysInOrder)
+        {
+            var stringBuilder = new StringBuilder(template);
+            var pos = 0;
+            var propertyKeys = new List<string>();
+            var currentParamsIndex = 0;
+
+            // convert the template into the normal {0,xxx:yyy} format and let string.Format handle the rest
+            while (pos < stringBuilder.Length)
+            {
+                // search for a '{'
+                if (stringBuilder[pos] == '{')
+                {
+                    ++pos;
+
+                    // make sure it's not escaped
+                    if (stringBuilder[pos] != '{')
+                    {
+                        var endPos = pos;
+                        while (stringBuilder[endPos] != '}')
+                        {
+                            if (endPos == stringBuilder.Length)
+                            {
+                                throw new FormatException("The given template is malformed.");
+                            }
+
+                            ++endPos;
+                        }
+
+                        var key = stringBuilder.ToString(pos, endPos - pos);
+                        var indexInString = currentParamsIndex.ToString(CultureInfo.InvariantCulture);
+                        propertyKeys.Add(key);
+                        stringBuilder.Replace(key, indexInString, pos, endPos - pos);
+                        pos += indexInString.Length;
+                        ++currentParamsIndex;
+                    }
+                }
+
+                ++pos;
+            }
+
+            convertedTemplate = stringBuilder.ToString();
+            propertyKeysInOrder = propertyKeys;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        internal static object ExtractObjectFromPropertyPath(string path, object root)
+        {
+            var obj = root;
+
+            foreach(var propertyName in path.Split('.'))
+            {
+                if (obj == null)
+                {
+                    throw new InvalidOperationException(string.Format(CultureInfo.InvariantCulture, "Cannot access '{0}' in null", propertyName));
+                }
+
+                var propertyInfo = obj.GetType().GetProperty(propertyName);
+                obj = propertyInfo.GetValue(obj);
+            }
+
+            return obj;
+        }
+    }
+}
