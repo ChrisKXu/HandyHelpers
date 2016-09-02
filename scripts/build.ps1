aa8bd581f1ff031c04ce631077dd173fbe72a163
Chris Xu <chxu@microsoft.com>
2016-09-02 13:50:03 -0700

    Upgrade to work with .NET Core 1.0

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,20 +1,14 @@
-# Setup dnx
-
-$Branch='dev'
-iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))
-dnvm install latest -r clr -arch x86 -alias default
-
 # Restore dependencies
-dnu restore --quiet
+dotnet restore --quiet
 
 # Build
-dnu build ./HandyHelpers/project.json ./HandyHelpers.Tests/project.json --quiet
+dotnet build ./HandyHelpers/project.json ./HandyHelpers.Tests/project.json --quiet
 
 # Test
-dnx -p HandyHelpers.Tests test
+dotnet -p HandyHelpers.Tests test
 
 # Pack
-dnu pack ./HandyHelpers/project.json --quiet --out ./artifacts --configuration Release
+dotnet pack ./HandyHelpers/project.json --quiet --out ./artifacts --configuration Release
 
 # All clear
 Write-Host "All clear!"
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env bash
 
-# Setup dnx
-curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh && source ~/.dnx/dnvm/dnvm.sh
-dnvm upgrade -r mono
-
 # Restore dependencies
-dnu restore --quiet
+dotnet restore --quiet
 
 # Build
-dnu build ./HandyHelpers/project.json ./HandyHelpers.Tests/project.json --quiet
+dotnet build ./HandyHelpers/project.json ./HandyHelpers.Tests/project.json --quiet
 
 # Test
 # Appending "-parallel none" to workaround a known bug that causes dnx to hang when using mono
 # See http://stackoverflow.com/questions/33581380/dnx-test-on-travis-with-mono-hangs
-dnx -p HandyHelpers.Tests test -parallel none
+dotnet -p HandyHelpers.Tests test -parallel none
 
 # All clear
 echo "All clear!"
